86098ab349d23fc1fb98d938d0bbc478a764143b
Syed Bilal Ahsan <bilalahsan402@gmail.com>
2022-05-14 21:40:27 +0500

    first commit

---
+++ lambdacode.py
@@ -0,0 +1,14 @@
+import json
+
+def lambda_handler(event, context):
+
+    # TODO implement
+
+    return {
+
+        'statusCode': 200,
+
+        'body': json.dumps('Updated Again Lambda Function!')
+
+    }
+
